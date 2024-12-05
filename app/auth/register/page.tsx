4cc7c31e3459416d34fc1bcb80369adee2270f2a
antonyjes <72809149@continental.edu.pe>
2024-12-04 19:42:58 -0500

    delete spanish comments

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -2,7 +2,14 @@
 
 import { CardWrapper } from "@/components/card-wrapper";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { RegisterSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,95 +19,111 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 const Register = () => {
-    const router = useRouter();
-    const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
-    const form = useForm<z.infer<typeof RegisterSchema>>({
-        resolver: zodResolver(RegisterSchema),
-        defaultValues: {
-            name: '',
-            email: '',
-            password: ''
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const registerUser = async (values: z.infer<typeof RegisterSchema>) => {
+    try {
+      startTransition(async () => {
+        const response = await fetch("/api/auth/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message || "An unexpected error occurred.");
         }
-    })
 
-    const registerUser = async (values: z.infer<typeof RegisterSchema>) => {
-        try {
-            // Activa el estado de transición (opcional, útil para UX)
-            startTransition(async () => {
-                const response = await fetch('/api/auth/register', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(values),
-                });
-    
-                // Manejar errores de la respuesta
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    throw new Error(errorData.message || 'An unexpected error occurred.');
-                }
-    
-                // Redirige al usuario a la página de inicio de sesión si la respuesta es exitosa
-                router.push('/auth/login');
-            });
-        } catch (error) {
-            console.error('Error during registration:', error);
-            alert('Something went wrong. Please try again.');
-        }
-    };
+        router.push("/auth/login");
+      });
+    } catch (error) {
+      console.error("Error during registration:", error);
+      alert("Something went wrong. Please try again.");
+    }
+  };
 
-    return (
-        <CardWrapper headerLabel="Create an account" backButtonLabel="Already have an account?" backButtonHref="/auth/login">
-            <Form {...form}>
-                <form className="grid gap-4" onSubmit={form.handleSubmit(registerUser)}>
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Name</FormLabel>
-                                <FormControl>
-                                    <Input {...field} disabled={isPending} placeholder="John Doe" type="text" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input {...field} disabled={isPending} placeholder="gW5lD@example.com" type="email" />
-                                </FormControl>
-                                <FormMessage /> 
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input {...field} disabled={isPending} placeholder="********" type="password" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button disabled={isPending} type="submit" className="w-full">
-                        Create account
-                    </Button>
-                </form>
-            </Form>
-        </CardWrapper>
-    )
-}
+  return (
+    <CardWrapper
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form className="grid gap-4" onSubmit={form.handleSubmit(registerUser)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    placeholder="John Doe"
+                    type="text"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    placeholder="gW5lD@example.com"
+                    type="email"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    placeholder="********"
+                    type="password"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isPending} type="submit" className="w-full">
+            Create account
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
 
 export default Register;
